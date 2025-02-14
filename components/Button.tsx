79dddd3ef573d7fb21e3bdd981e999c790071e3b
Ruan <ruancampossilvaa23@gmail.com>
2025-02-14 08:34:25 -0300

    inst expo img-pick

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,37 @@
+import { FontAwesome } from "@expo/vector-icons";
 import { StyleSheet, View, Pressable, Text } from "react-native";
 
 type Props = {
   label: string;
+  theme?: "primary"; //? optional
 };
 
-export default function Button({ label }: Props) {
+export default function Button({ label, theme }: Props) {
+  if (theme === "primary") {
+    return (
+      <View
+        style={[
+          styles.buttonContainer,
+          { borderWidth: 4, borderColor: "#FFD33D", borderRadius: 18 },
+        ]}
+      >
+        <Pressable
+            style={[styles.button, { backgroundColor: "#FFF" }]}
+            onPress={() => alert("voce clicou aqui")}
+        >
+            <FontAwesome
+                name="picture-o"
+                size={18}
+                color={"#25292E"}
+                style={styles.buttonIcon}
+            />
+          <Text style={[styles.buttonLabel, {color: "#25292E"}]}>
+            {label}
+          </Text>
+        </Pressable>
+      </View>
+    );
+  }
   return (
     <View style={styles.buttonContainer}>
       <Pressable onPress={() => alert("voce clicou aqui")}>
@@ -14,9 +41,7 @@ export default function Button({ label }: Props) {
   );
 }
 
-const styles=
-
-StyleSheet.create({
+const styles = StyleSheet.create({
   buttonContainer: {
     width: 320,
     height: 68,
@@ -31,10 +56,13 @@ StyleSheet.create({
     height: "100%",
     alignItems: "center",
     justifyContent: "center",
-    flexDirection: "row"
+    flexDirection: "row",
   },
-  buttonLabel:{
-    color:"#FFF",
-    fontSize:16,
-},
+  buttonLabel: {
+    color: "#FFF",
+    fontSize: 16,
+  },
+  buttonIcon: {
+    paddingRight: 8,
+  }
 });
